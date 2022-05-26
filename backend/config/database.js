38699a966bb67439c381fb9e64794d66483aed25
Jai Lakhmani <jailakhmani12345@gmail.com>
2022-05-26 17:10:57 +0530

    Update 6.4

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_host' ),
-      port: env.int('DATABASE_port'),
-      database: env('DATABASE_database'),
-      user: env('DATABASE_username'),
-      password: env('DATABASE_password'),
-      ssl: env.bool('DATABASE_sslmode'),
+      host: env('DATABASE_host', "db-postgresql-blr1-71263-do-user-11624818-0.b.db.ondigitalocean.com" ),
+      port: env.int('DATABASE_port', 25060),
+      database: env('DATABASE_database', "defaultdb"),
+      user: env('DATABASE_username', "doadmin"),
+      password: env('DATABASE_password', "AVNS_W91i24LNQ224Ryf hide"),
+      schema: env('DATABASE_schema', "public")
     },
   },
 });
