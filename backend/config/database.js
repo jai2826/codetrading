5a631aed95afd64c6420386289affb64f4a6c3e4
Jai Lakhmani <jailakhmani12345@gmail.com>
2022-05-26 17:27:29 +0530

    update 6.5

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_host', "db-postgresql-blr1-71263-do-user-11624818-0.b.db.ondigitalocean.com" ),
-      port: env.int('DATABASE_port', 25060),
-      database: env('DATABASE_database', "defaultdb"),
-      user: env('DATABASE_username', "doadmin"),
-      password: env('DATABASE_password', "AVNS_W91i24LNQ224Ryf hide"),
-      schema: env('DATABASE_schema', "public")
+      host: env('host', '127.0.0.1'),
+      port: env.int('port', 5432),
+      database: env('database', 'codeblogs'),
+      user: env('username', 'postgres'),
+      password: env('password', 'Jaip2826@'),
+      ssl: env.bool('sslmode', false),
     },
   },
 });
