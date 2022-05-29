1e3b7232782d4600d8fb3058f4fc90b5f083a33b
Jai Lakhmani <jailakhmani12345@gmail.com>
2022-05-29 21:35:38 +0530

    Update 7.1
    Fixed some image issues in next.config.js

--- codeblogs/.next/static/chunks/webpack.js
+++ codeblogs/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "386f3db05b821472"; }
+/******/ 		__webpack_require__.h = function() { return "bdf6613182fff46d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- codeblogs/next.config.js
+++ codeblogs/next.config.js
@@ -4,9 +4,9 @@ module.exports = {
     env:{
       IMAGES_DOMAIN:process.env.IMAGES_DOMAIN
     },
-    publicRuntimeConfig:{
-      IMAGES_DOMAIN:process.env.IMAGES_DOMAIN
-    },
+    // publicRuntimeConfig:{
+    //   IMAGES_DOMAIN:process.env.IMAGES_DOMAIN
+    // },
     images: {
           domains: [process.env.IMAGES_DOMAIN],
         },
