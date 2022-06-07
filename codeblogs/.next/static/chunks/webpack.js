f5d751bd7c9015fd32018183cd0860b17b2d4c22
Jai Lakhmani <jailakhmani12345@gmail.com>
2022-06-07 12:03:04 +0530

    Update 14
    Added Sitemap

--- codeblogs/.next/static/chunks/webpack.js
+++ codeblogs/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc656c727a4b3ca5"; }
+/******/ 		__webpack_require__.h = function() { return "fee5416b51dadd34"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
