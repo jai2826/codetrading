0344a27f800a99e4c0614b99c8f55ae72ecbbb65
Jai Lakhmani <jailakhmani12345@gmail.com>
2022-06-14 21:47:56 +0530

    Update 17.5
    Fixed some previous css isuues

--- geekcell/.next/static/chunks/webpack.js
+++ geekcell/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1968d99ca46846c5"; }
+/******/ 		__webpack_require__.h = function() { return "ecb502d1450e6bf2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
