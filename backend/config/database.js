2b1fd1718bc05e37935b979962b5e753098af70a
Jai Lakhmani <81863299+jai2826@users.noreply.github.com>
2022-07-07 21:52:56 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -12,5 +12,16 @@ module.exports = ({ env }) => ({
       },
       sslmode: env('sslmode', "require")
     },
-  },
+    acquireConnectionTimeout: 10000,
+    pool: {
+      min: 0,
+      max: 1,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 300000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis:1000,
+      createRetryIntervalMillis: 2000
+    },
+},
 });
